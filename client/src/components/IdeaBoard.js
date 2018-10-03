82e8e3ed3817144e3deebeba461895cd4d65e4f6
AndrewBantug <AndrewBantug@gmail.com>
2018-10-03 14:33:15 -0400

    react app takes data from express api

--- client/src/components/IdeaBoard.js
+++ client/src/components/IdeaBoard.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class IdeaBoard extends Component {
+  state = {
+    response: {}
+  }
+
+  getUsers = () => {
+    axios.get('/api/users')
+      .then(response => console.log(response))
+  }
+
+  componentDidMount = () => {
+    this.getUsers()
+  }
+
   render() {
     return (
       <div>
